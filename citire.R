8b0768288895a03e730537e496e827b04f36c339
Ana Maria Antoaneta Tiru <Ana.Tiru@insse.ro>
2019-04-19 12:27:32 +0300

    add substr_errors function

--- citire.R
+++ citire.R
@@ -33,6 +33,19 @@ year2to4 <- function (cnp) {
   return (year)
 }
 
+substr_errors <- function (cnp, start, stop) {
+  col <- c()
+  for (i in 1:length(cnp)) {
+    if (is.na(cnp[i]) | cnp[i] == 0 | nchar(cnp[i]) != 13) {
+      col <- c(col, NA)
+      next
+    }
+    col <- c(col, substr(cnp[i], start = start, stop = stop))
+  }
+  col <- as.integer(col)
+  return (col)
+}
+
 # SEXCRP, ANCRP, LUNACRP, ZICRP
 add_columns <- function (df) {
   column_names <- names(df)
@@ -53,10 +66,10 @@ add_columns <- function (df) {
       lunacrp <- paste0("LUNACRP", F_M)
       zicrp <- paste0("ZICRP", F_M)
     }
-    df[,sexcrp] <- as.integer(substr(df[,i], start = 1, stop = 1))
+    df[,sexcrp] <- substr_errors(df[,i], start = 1, stop = 1)
     df[,ancrp] <- year2to4(df[,i])
-    df[,lunacrp] <- as.integer(substr(df[,i], start = 4, stop = 5))
-    df[,zicrp] <- as.integer(substr(df[,i], start = 6, stop = 7))
+    df[,lunacrp] <- substr_errors(df[,i], start = 4, stop = 5)
+    df[,zicrp] <- substr_errors(df[,i], start = 6, stop = 7)
   }
   return(df)
 }
@@ -76,11 +89,13 @@ encrypt <- function (df) {
   if (nrow(df) == 0) return (df) 
   df <- add_columns(df)
   column_names <- names(df)
-  column_names <- column_names[-grep("dns", tolower(column_names))]
   cnp <- grep("cnp", tolower(column_names))
+  if (length(cnp) == 0) return (df) 
   cnp_colname <- column_names[cnp] 
   for (i in cnp_colname) {
+    colname <- gsub(pattern = "CNP", replacement = "ISU", x = i, ignore.case = TRUE)
     df[,i] <- unlist(lapply(df[,i], digest_if))
+    colnames(df)[which(names(df) == i)] <- colname
   }
   return (df)
 }
@@ -110,57 +125,3 @@ for (i in folders) {
 }
 
 
-#testare =================
-setwd("D:/ANA_MARIA/demografie/testare_citire_R/new/dem_3B01")
-
-files <- list.files()
-dbf <- grep(".dbf", files)
-files <- files[dbf]
-
-a = lapply(files, function(x) read.dbf(file=x)) # citire toate fisierele dbf
-
-# citirea datelor: probleme! adauga 8 campuri in plus goale
-x1 <- read.dbf("cs_3B01.dbf")
-x2 <- read.dbf("dc_3B01.dbf")
-x3 <- read.dbf("dv_3B01.dbf")
-x4 <- read.dbf("nm_3B01.dbf")
-x5 <- read.dbf("nv_3B01.dbf")
-
-# verifica tipurile de date
-datetype <- c()
-for (i in 1:ncol(x)) {
-  datetype <- c(datetype, class(x[,i]))
-}
-
-# verifica daca exista x in numele de coloane (8)
-length(grep("x", tolower(names(x1))))
-length(grep("x", tolower(names(x2))))
-length(grep("x", tolower(names(x3))))
-length(grep("x", tolower(names(x4))))
-length(grep("x", tolower(names(x5))))
-# rezultat: mai sunt si alte coloane ce contin x
-
-# warning
-tt <- tryCatch(read.dbf("cs_3B01.dbf"),error=function(e) e, warning=function(w) w)
-if(is(tt,"warning")) print("KOOKOO")
-
-name <- c(" an ",	"luna",	"nrbul",	"judet_i",	"sector_i",
-          "loca_i",	"mediu_i",	"data_i",	"nract_i",	"cnp_m",
-          "cnp_f",	"datan_m",	"vcasa_m",	"vcasl_m",	"vcasz_m",
-          "datan_f",	"vcasa_f",	"vcasl_f",	"vcasz_f",	"stciv_m",
-          "stciv_f",	"nrcas_m",	"nrcas_f",	"activ_m",	"activ_f",	"scoala_m",
-          "scoala_f",	"cet_m",	"cet_f",	"nat_m",	"nat_f",	"r", "", "")
-mn <- make.names(name, unique = TRUE)
-# scrierea datelor: probleme! 
-write.dbf(x, "x.dbf")
-
-
-# criptarea
-c1 <- toupper(digest("1740714411519", algo = "sha1", serialize=FALSE))
-identical(c1, "F9B2247F88352BCAFFAF66C91295B824C5FA6C76")
-digest(kd$CNP_F[1:nrow(kd)], algo = "sha1", serialize=FALSE)
-
-
-lapply(kd$CNP_M, dg)
-column_names <- names(x1)
-column_names <- column_names[,-grep("dns", tolower(column_names))]
